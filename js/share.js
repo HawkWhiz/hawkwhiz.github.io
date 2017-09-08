7ad47c109d67e317b063bb4555ed910e82a461dc
HawkWhiz <31573041+HawkWhiz@users.noreply.github.com>
2017-09-08 18:11:43 +0530

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
-
     $("#more_share").click(function(){
         $("#share_buttons").show();
         $("#more_share").hide();
+        
     });
     $("#less_share").click(function(){
         $("#share_buttons").hide();
         $("#more_share").show();
     });
+
+
 });
