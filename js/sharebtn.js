0f1c3e6e1a67f6e5aa221bd972750cd95b954ede
HawkWhiz <31573041+HawkWhiz@users.noreply.github.com>
2017-09-14 10:06:45 +0530

    Add files via upload

--- js/sharebtn.js
+++ js/sharebtn.js
@@ -26,3 +26,7 @@ function shareLinkedin(){
           function sharePinterest(){
             var link = "https://pinterest.com/pin/create/bookmarklet/?media=" + encodePicture + "&url=" + encodeURL + "&description=" + encodeTitle;
             window.open(link);}
+function sharePinterestPicture(picFile){
+    var encodePinPic = encodeURIComponent("https://hawkwhiz.github.io/images/" + "picFile");
+    var link = "https://pinterest.com/pin/create/bookmarklet/?media=" + encodePinPic + "&url=" + encodeURL + "&description=" + encodeTitle;
+}
