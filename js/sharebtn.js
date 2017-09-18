72c382662d1a01923cccff4fad19c6fd1c0c2221
HawkWhiz <31573041+HawkWhiz@users.noreply.github.com>
2017-09-18 20:50:46 +0530

    Add files via upload

--- js/sharebtn.js
+++ js/sharebtn.js
@@ -2,32 +2,32 @@ var encodeURL = encodeURIComponent(url),
 encodeTitle = encodeURIComponent(title),
 encodePicture = encodeURIComponent(picture);
 
-function shareFacebook(){
-  var link="https://www.facebook.com/sharer/sharer.php?u=" + encodeURL;
+function shareFacebook(U){
+  var link="https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(U);
   window.open(link);}
 
-function shareTwitter(){
-var link = "https://twitter.com/home?status="+ encodeURL
-;
+function shareTwitter(U){
+var link = "https://twitter.com/home?status="+ encodeURIComponent(U);
 window.open(link);}
 
-function shareLinkedin(){
-  var link = "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURL;
+function shareLinkedin(U){
+  var link = "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(U);
   window.open(link);}
 
-    function shareReddit(){
-      var link="http://reddit.com/submit?url="+encodeURL+"&title="+encodeTitle;
+    function shareReddit(U, T){
+      var link="http://reddit.com/submit?url=" + encodeURIComponent(U) + "&title=";
       window.open(link);}
 
-        function shareDigg(){
-          var link = "http://digg.com/submit?url="+ encodeURL + "&title=" + encodeTitle;
+        function shareDigg(U,T){
+          var link = "http://digg.com/submit?url=" + encodeURIComponent(U) + "&title=" + encodeURIComponent(T);
           window.open(link);}
 
-          function sharePinterest(){
-            var link = "https://pinterest.com/pin/create/bookmarklet/?media=" + encodePicture + "&url=" + encodeURL + "&description=" + encodeTitle;
+          function sharePinterest(P,U,T){
+            var link = "https://pinterest.com/pin/create/bookmarklet/?media=" + encodeURIComponent(P) + "&url=" + encodeURIComponent(U) + "&description=" + encodeURIComponent(T);
             window.open(link);}
-function sharePinterestPicture(picFile){
+
+/*function sharePinterestPicture(picFile){
     var encodePinPic = encodeURIComponent("https://hawkwhiz.github.io/images/" + "picFile");
     var link = "https://pinterest.com/pin/create/bookmarklet/?media=" + encodePinPic + "&url=" + encodeURL + "&description=" + encodeTitle;
     window.open(link);
-}
+}*/
